dc397074080b5907d7009b6cc87e7fc0b749b590
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-16 15:46:01 +0800

    Add: foreign-dictionary - submission-2

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adjList = {c : set() for w in words for c in w}
+        indegree = {c : 0 for c in adjList}
+
+
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+            minLen = min(len(w1), len(w2))
+            # if string before longer and same prefix..
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adjList[w1[j]]:    
+                        adjList[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
+                    break
+        
+        q = deque([c for c in indegree if indegree[c] == 0])
+        res = []
+
+        while q:
+            char = q.popleft()
+            res.append(char)
+            for neighbor in adjList[char]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        if len(res) != len(indegree):
+            return ""
+
+        return "".join(res)
