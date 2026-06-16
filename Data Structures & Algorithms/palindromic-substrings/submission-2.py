be4ec4fca2fab269c4a99a99eae74b75f2f6f052
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-16 17:17:47 +0800

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        # optimal substructure
+        # if i == j: check dp(i+1, j-1)
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        res = 0
+
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i + 1 <= 2 or dp[i+1][j-1]):
+                    dp[i][j] = True
+                    res += 1
+        
+        return res
