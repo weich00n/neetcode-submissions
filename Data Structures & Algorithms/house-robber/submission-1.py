f8bb40d233bf26daa80c44741f25a040968e13ef
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-16 16:45:53 +0800

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # optimal substucture
+        # rob house and dont rob before, or just try next
+        n = len(nums)
+        dp = [0] * (n + 1)
+
+        dp[1] = nums[0]
+
+        for i in range(2, n+1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+
+        return dp[n]
