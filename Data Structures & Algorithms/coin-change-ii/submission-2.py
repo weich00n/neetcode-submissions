3612a4624431cbba125754e237ea7b4a6618ade0
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-18 13:58:34 +0800

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        n = len(coins)
+        dp = [0 for _ in range(amount + 1)]
+
+        dp[0] = 1
+        
+        for coin in coins:
+            for i in range(1, amount + 1):
+                if i >= coin:
+                    dp[i] += dp[i-coin]
+        
+        return dp[amount]
