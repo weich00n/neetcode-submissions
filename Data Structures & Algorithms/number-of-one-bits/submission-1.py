1510175f869b8eb60757ee15bc07da65701395f2
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-24 13:43:39 +0800

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+
+        while n > 0:
+            res += 1 if n & 1 == 1 else 0
+            n >>= 1
+        
+        return res
