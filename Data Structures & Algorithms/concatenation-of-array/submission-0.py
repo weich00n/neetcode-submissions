30dd352de0ba70202275d461841a8ce721a80194
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-25 14:41:11 +0800

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * (2*n)
+        for i in range(n):
+            res[i] = res[i+n] = nums[i]
+        
+        return res
