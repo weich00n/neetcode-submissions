92c208675c2b67cb0cae279e199da04793867b1c
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-25 13:25:13 +0800

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        res = n
+        for i in range(n):
+            res ^= nums[i]
+            res ^= i
+        
+        return res
