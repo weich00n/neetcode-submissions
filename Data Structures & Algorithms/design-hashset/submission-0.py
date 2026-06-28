9712a19eacb8fc03465b6276cac6fec7c21e6c41
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-28 16:11:39 +0800

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,46 @@
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.set = [ListNode(0) for i in range(10000)]
+
+    def add(self, key: int) -> None:
+        idx = key % len(self.set)
+        curr = self.set[idx]
+
+        while curr.next:
+            if curr.next.key == key:
+                return
+            curr = curr.next
+            
+        curr.next = ListNode(key)
+
+    def remove(self, key: int) -> None:
+        curr = self.set[key%len(self.set)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+    def contains(self, key: int) -> bool:
+        idx = key % len(self.set)
+        curr = self.set[idx]
+
+        while curr.next:
+            if curr.next.key == key:
+                return True
+            curr = curr.next
+        
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
