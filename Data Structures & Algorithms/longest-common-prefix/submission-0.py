14b770fe91b9603e19e4251a38f89cfd151ae183
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-28 13:16:58 +0800

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        strs.sort()
+
+        # compare the first and last because they are all common
+        
+        i = 0
+        j = 0
+
+        res = []
+
+        while i < len(strs[0]) and j < len(strs[-1]):
+            if strs[0][i] == strs[-1][j]:
+                res.append(strs[0][i])
+                i += 1
+                j += 1
+            else:
+                break
+        
+        return "".join(res)
