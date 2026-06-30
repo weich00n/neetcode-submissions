dabe3985b703fa8e6c971d2389eb8d4f719375ac
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-06-30 12:16:44 +0800

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def isAlNum(self,c):
+        return ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9') or ord('A') <= ord(c) <= ord('Z') 
+
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            while l < r and not self.isAlNum(s[l]):
+                l += 1
+            
+            while l < r and  not self.isAlNum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+
+        return True
+            
