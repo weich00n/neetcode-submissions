058c5c0c04269d8bfab72e5a5b8bcc2200faaa99
Chong Wei Choon <weic.chong.2024@computing.smu.edu.sg>
2026-09-05 16:54:11 +0800

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        map = {')': '(', '}' : '{', ']':'['}
+        for c in s:
+            if c in map:
+                if stack and stack[-1] == map[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return not stack
